485cdc3484b483f219077276b9d73fe5f10a3e0b
Matiar <matiroson@gmail.com>
2022-08-08 15:02:07 -0300

    fix: url doc on footer (#12)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -119,7 +119,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/intro',
+                to: '/docs',
               },
             ],
           },
