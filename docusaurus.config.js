197f580c9e62806db29b5c231484d698ff02b0c0
Matias I <izquierdiito@gmail.com>
2024-08-13 14:39:26 -0300

    chore: change whole provider docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,38 +1,39 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'SleakOps Documentation',
-  tagline: 'SleakOps Documentation',
-  url: 'https://docs.sleakops.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  title: "SleakOps Documentation",
+  tagline: "SleakOps Documentation",
+  url: "https://docs.sleakops.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Craftech', // Usually your GitHub org/user name.
-  projectName: 'Sleakops', // Usually your repo name.
-  deploymentBranch: 'gh-pages',
+  organizationName: "Craftech", // Usually your GitHub org/user name.
+  projectName: "Sleakops", // Usually your repo name.
+  deploymentBranch: "gh-pages",
   trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   themes: [
     [
       require.resolve("@easyops-cn/docusaurus-search-local"),
-      { // https://github.com/easyops-cn/docusaurus-search-local#theme-options
+      {
+        // https://github.com/easyops-cn/docusaurus-search-local#theme-options
         // ... Your options.
         // `hashed` is recommended as long-term-cache of index file is possible.
         hashed: true,
@@ -44,40 +45,39 @@ const config = {
     ],
   ],
 
-
   presets: [
     [
-      'classic',
+      "classic",
       {
         // Debug defaults to true in dev, false in prod
         debug: true,
         // Will be passed to @docusaurus/theme-classic.
         theme: {
-          customCss: [require.resolve('./src/css/custom.css')],
+          customCss: [require.resolve("./src/css/custom.css")],
         },
         // Will be passed to @docusaurus/plugin-content-docs (false to disable)
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          routeBasePath: '/',
+          sidebarPath: require.resolve("./sidebars.js"),
+          routeBasePath: "/",
         },
         // Will be passed to @docusaurus/plugin-content-blog (false to disable)
         blog: {},
         // Will be passed to @docusaurus/plugin-content-pages (false to disable)
         pages: {
-          routeBasePath: '/pages',
+          routeBasePath: "/pages",
         },
         // Will be passed to @docusaurus/plugin-content-sitemap (false to disable)
         sitemap: {
-          changefreq: 'weekly',
+          changefreq: "weekly",
           priority: 0.5,
-          ignorePatterns: ['/tags/**'],
-          filename: 'sitemap.xml',
+          ignorePatterns: ["/tags/**"],
+          filename: "sitemap.xml",
         },
         // Will be passed to @docusaurus/plugin-google-gtag (only enabled when explicitly specified)
         // gtag: {},
         // Will be passed to @docusaurus/plugin-google-analytics (only enabled when explicitly specified)
         googleAnalytics: {
-          trackingID: 'UA-128497858-1',
+          trackingID: "UA-128497858-1",
           anonymizeIP: true,
         },
       },
@@ -88,15 +88,15 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       colorMode: {
-        defaultMode: 'dark',
+        defaultMode: "dark",
         disableSwitch: false,
         respectPrefersColorScheme: false,
       },
       navbar: {
-        title: 'SleakOps',
+        title: "SleakOps",
         logo: {
-          alt: 'SleakOps',
-          src: 'img/logo.svg',
+          alt: "SleakOps",
+          src: "img/logo.svg",
         },
         items: [
           // {
@@ -107,31 +107,30 @@ const config = {
           // },
           // {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://console.sleakops.com',
-            label: 'Console',
-            position: 'right',
+            href: "https://console.sleakops.com",
+            label: "Console",
+            position: "right",
           },
         ],
-        
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Tutorial',
-                to: '/',
+                label: "Tutorial",
+                to: "/",
               },
               {
                 label: "About us",
-                href: "https://sleakops.com/"
+                href: "https://sleakops.com/",
               },
               {
                 label: "Contact us",
-                href: "https://sleakops.com/contact"
-              }
+                href: "https://sleakops.com/contact",
+              },
             ],
           },
           // {
@@ -152,15 +151,15 @@ const config = {
           //   ],
           // },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'Terms & Conditions',
-                href: 'https://craftech.notion.site/Terms-Conditions-efb8d9a980654625af456732ed44b197',
+                label: "Terms & Conditions",
+                href: "https://craftech.notion.site/Terms-Conditions-efb8d9a980654625af456732ed44b197",
               },
               {
-                label: 'Privacy Policy',
-                href: 'https://craftech.notion.site/Privacy-Policy-of-SleakOps-55b977da50d84871809879d065b16b3b',
+                label: "Privacy Policy",
+                href: "https://craftech.notion.site/Privacy-Policy-of-SleakOps-55b977da50d84871809879d065b16b3b",
               },
             ],
           },
