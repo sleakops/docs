12418fda65e44e43d49fedd92d7edf016aa3f2c2
Matías Izquierdo <61392581+fuzilogik2019@users.noreply.github.com>
2026-01-12 12:48:32 -0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
   tagline: "SleakOps Documentation",
   url: "https://docs.sleakops.com",
   baseUrl: process.env.BASE_URL || "/",
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
 
